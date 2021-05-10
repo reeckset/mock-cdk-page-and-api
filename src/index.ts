ededcef4f53010a844c4fdcfe8ab2ff6538bb4fd
Henrique Lima <henlima@amazon.com>
2021-05-10 09:44:01 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3deploy from '@aws-cdk/aws-s3-deployment';
+import * as apigateway from '@aws-cdk/aws-apigateway';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+import * as cdk from '@aws-cdk/core';
+
+interface Props {
+  pathToS3Content: string
+}
+
+export class WebpageWithAPI extends cdk.Construct {
+
+  public readonly api: apigateway.RestApi;
+
+  constructor(scope: cdk.Construct, id: string, props: Props) {
+    super(scope, id);
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      websiteIndexDocument: 'index.html',
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(props.pathToS3Content)],
+      destinationBucket: websiteBucket,
+      destinationKeyPrefix: 'web/static' // optional prefix in destination bucket
+    });
+
+    new cloudfront.Distribution(this, 'myDist', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) }
+    });
+    
+    this.api = new apigateway.RestApi(this, 'api');
+  }
+}
